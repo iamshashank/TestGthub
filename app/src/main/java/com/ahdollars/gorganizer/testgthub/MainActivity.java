bb8d31d72b1754a6a6dfeb85e83d121d7b7abc7a
akashsharms <akashsharms@gmail.com>
2017-04-13 23:29:02 +0530

    0.4

--- app/src/main/java/com/ahdollars/gorganizer/testgthub/MainActivity.java
+++ app/src/main/java/com/ahdollars/gorganizer/testgthub/MainActivity.java
@@ -4,12 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+Boolean f=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //nike
         //jjhjhj
+
     }
 }
